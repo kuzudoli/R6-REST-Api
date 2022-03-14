d52404f25d111084c4aa63de541af4db02e5cf1d
kuzudoli <enescakkr@windowslive.com>
2022-03-14 11:59:48 +0300

    Add home view

---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+function quick_search(){
+    var content = document.getElementById("search").value;
+    if(content == '')
+        content = 'https://r6-rest-api.herokuapp.com/operators/1/';
+    
+    var call_url = 'https://r6-rest-api.herokuapp.com/api/operators?name=' + content;
+    
+    jQuery.ajax({
+      url: call_url,
+      dataType: 'json',
+      context: document.body
+    }).complete(function(data){
+        if(data['status'] == 200){
+            var d = jQuery.parseJSON(data['responseText']);
+            jQuery('#request_output').text(JSON.stringify(d, null, '\t'));
+        }
+        else if (data['status'] == 404) {
+            jQuery('#request_output').text(data['status'] + ' ' + data['statusText']);
+        }
+    });
+}
+    
